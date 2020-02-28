72ac6a7838c45313ebd7e8d44d0ba6e8d5bd968c
git-aditi <aditi.shrivastava@students.iiit.ac.in>
2020-02-29 03:40:06 +0530

    Developed Initial Activity pages

---
+++ app/src/main/java/com/example/mymess/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mymess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+//    String description = "This is a description";
+    EditText editText;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namevalue = editText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SelectPreferences.class);
+                intent.putExtra("NAME", namevalue);
+//                intent.putExtra("DESCRIPTION", description);
+                startActivity(intent);
+            }
+        });
+    }
+}
