7e19e9f69cfa22c8edd593877898654b487cfff5
git-aditi <aditi.shrivastava@students.iiit.ac.in>
2020-05-20 01:27:24 +0530

    Tags selected by the user in selectpreferences activity passed to showpreferences activity as an array

--- app/src/main/java/com/example/mymess/SelectPreferences.java
+++ app/src/main/java/com/example/mymess/SelectPreferences.java
@@ -6,21 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SelectPreferences extends AppCompatActivity {
-
+    Button btn;
+    private static final String TAG = "SelectPreferences";
     TextView heading, heading_label;
 //    Button btn;
     Button mShowMessChoices;
     TextView mDisplaySelected;
     String[] listItems;
     boolean[] checkedItems;
+    ArrayList<String> userSelectedTags = new ArrayList<>();
     ArrayList<Integer> mUserItems = new ArrayList<>();
 
     @Override
@@ -60,10 +66,14 @@ public class SelectPreferences extends AppCompatActivity {
                         for(int i = 0; i < mUserItems.size(); i++){
                             item = item + listItems[mUserItems.get(i)];
                             if(i != mUserItems.size() - 1){
-                                item = item + ", ";
+                                item = item + ",";
                             }
                         }
                         mDisplaySelected.setText(item);
+
+
+
+
                     }
                 });
 
@@ -90,42 +100,20 @@ public class SelectPreferences extends AppCompatActivity {
             }
         });
 
-//        btn = (Button) findViewById(R.id.foodPreferencesButton);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                int count = 0;
-//                String categoryChoices = "";
-//                boolean spicyChecked = ((CheckBox) findViewById(R.id.checkBoxSpicy)).isChecked();
-//                boolean healthyChecked = ((CheckBox) findViewById(R.id.checkBoxHealthy)).isChecked();
-//                boolean fibrousChecked = ((CheckBox) findViewById(R.id.checkBoxFibrous)).isChecked();
-//                boolean friedChecked = ((CheckBox) findViewById(R.id.checkBoxFried)).isChecked();
-//                if (spicyChecked) {
-//                    categoryChoices += "Spicy, ";
-//                    count += 1;
-//                }
-//                if (healthyChecked) {
-//                    categoryChoices += "Healthy, ";
-//                    count += 1;
-//                }
-//                if (fibrousChecked) {
-//                    categoryChoices += "Fibrous, ";
-//                    count += 1;
-//                }
-//                if (friedChecked) {
-//                    categoryChoices += "Fried, ";
-//                    count += 1;
-//                }
-//                if (count > 0) {
-//                    categoryChoices = categoryChoices.substring(0, categoryChoices.length() - 2);
-//                } else {
-//                    categoryChoices = "None";
-//                }
-//                Intent intent = new Intent(SelectPreferences.this, ShowPreferences.class);
-//                intent.putExtra("CATEGORIES", categoryChoices);
-//                intent.putExtra("DESCRIPTION", description);
-//                startActivity(intent);
-//            }
-//        });
+
+        btn = (Button) findViewById(R.id.submitBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i = 0; i < mUserItems.size(); i++) {
+                    String s = listItems[mUserItems.get(i)];
+                    userSelectedTags.add(s);
+                }
+                Intent intent = new Intent(SelectPreferences.this, ShowPreferences.class);
+                intent.putExtra("USER_SELECTED_TAGS", userSelectedTags);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mymess/ShowPreferences.java
+++ app/src/main/java/com/example/mymess/ShowPreferences.java
@@ -1,10 +1,13 @@
 package com.example.mymess;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class ShowPreferences extends AppCompatActivity {
 
@@ -15,7 +18,14 @@ public class ShowPreferences extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_preferences);
 
+        ArrayList<String> userSelectedTags = (ArrayList<String>) getIntent().getSerializableExtra("USER_SELECTED_TAGS");
+        String s = "";
+        for (String i : userSelectedTags){
+            System.out.println(i);
+            s += i;
+            s+= ", ";
+        }
         heading = findViewById(R.id.headingLabel);
-        heading.setText("You chose: " + getIntent().getStringExtra("CATEGORIES"));
+        heading.setText(s);
     }
 }
